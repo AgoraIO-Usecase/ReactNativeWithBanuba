68966bee45b5583278fc4b9d8d1c8a40774355c0
HUI <luxuhui@agora.io>
2023-11-14 21:52:26 +0800

    feat: support banuba extension

--- App.tsx
+++ App.tsx
@@ -5,55 +5,39 @@
  * @format
  */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, {useCallback, useEffect, useRef, useState} from 'react';
 import {
+  PermissionsAndroid,
+  Platform,
   SafeAreaView,
-  ScrollView,
   StatusBar,
   StyleSheet,
-  Text,
   useColorScheme,
-  View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+import createAgoraRtcEngine, {
+  ChannelProfileType,
+  ClientRoleType,
+  RtcSurfaceView,
+  VideoSourceType,
+} from 'react-native-agora';
 
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+import {MainBundlePath} from 'react-native-fs';
+
+import {useAsyncEffect} from 'use-async-effect';
+
+// Banuba Constants
+export const BNBKeyVendorName = 'Banuba';
+export const BNBKeyExtensionName = 'BanubaFilter';
+export const BNBKeyLoadEffect = 'load_effect';
+export const BNBKeyUnloadEffect = 'unload_effect';
+export const BNBKeySetBanubaLicenseToken =
+  Platform.OS === 'android' ? 'initialize' : 'set_banuba_license_token';
+export const BNBKeySetEffectsPath = 'set_effects_path';
+export const BNBKeyEvalJSMethod = 'eval_js';
+export const banubaLicenseToken = 'YOUR_BANUBA_LICENSE_TOKEN';
 
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -62,56 +46,105 @@ function App(): JSX.Element {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
+  const engine = useRef(createAgoraRtcEngine());
+  const [joinChannelSuccess, setJoinChannelSuccess] = useState(false);
+
+  const onJoinChannelSuccess = useCallback(() => {
+    engine.current.setExtensionProperty(
+      BNBKeyVendorName,
+      BNBKeyExtensionName,
+      BNBKeyLoadEffect,
+      'TrollGrandma',
+    );
+    setJoinChannelSuccess(true);
+  }, []);
+  const onLeaveChannel = useCallback(() => {
+    engine.current.setExtensionProperty(
+      BNBKeyVendorName,
+      BNBKeyExtensionName,
+      BNBKeyUnloadEffect,
+      'TrollGrandma',
+    );
+    setJoinChannelSuccess(false);
+  }, []);
+
+  useEffect(() => {
+    engine.current.addListener('onJoinChannelSuccess', onJoinChannelSuccess);
+    engine.current.addListener('onLeaveChannel', onLeaveChannel);
+
+    const engineCopy = engine.current;
+    return () => {
+      engineCopy.removeListener('onJoinChannelSuccess', onJoinChannelSuccess);
+      engineCopy.removeListener('onLeaveChannel', onLeaveChannel);
+    };
+  }, [onJoinChannelSuccess, onLeaveChannel]);
+
+  useAsyncEffect(async () => {
+    if (Platform.OS === 'android') {
+      await PermissionsAndroid.requestMultiple([
+        'android.permission.RECORD_AUDIO',
+        'android.permission.CAMERA',
+      ]);
+    }
+
+    engine.current.initialize({
+      appId: 'YOUR_AGORA_APP_ID',
+      channelProfile: ChannelProfileType.ChannelProfileLiveBroadcasting,
+    });
+    if (Platform.OS === 'android') {
+      engine.current.loadExtensionProvider('banuba-plugin');
+    }
+    engine.current.enableExtension(BNBKeyVendorName, BNBKeyExtensionName, true);
+    engine.current.enableVideo();
+    engine.current.setClientRole(ClientRoleType.ClientRoleBroadcaster);
+    engine.current.startPreview();
+
+    if (Platform.OS === 'ios') {
+      engine.current.setExtensionProperty(
+        BNBKeyVendorName,
+        BNBKeyExtensionName,
+        BNBKeySetEffectsPath,
+        MainBundlePath,
+      );
+    }
+
+    engine.current.setExtensionProperty(
+      BNBKeyVendorName,
+      BNBKeyExtensionName,
+      BNBKeySetBanubaLicenseToken,
+      banubaLicenseToken,
+    );
+
+    engine.current.joinChannel('', 'lxh', 0, {});
+
+    const engineCopy = engine.current;
+    return () => {
+      engineCopy.leaveChannel();
+      engineCopy.release();
+    };
+  }, []);
+
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+      {joinChannelSuccess ? (
+        <RtcSurfaceView
+          style={styles.preview}
+          canvas={{uid: 0, sourceType: VideoSourceType.VideoSourceCamera}}
+        />
+      ) : undefined}
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  preview: {
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'black',
   },
 });
 
